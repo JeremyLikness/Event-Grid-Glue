b764d7882e2d1867bc93445851c5f3e11407f667
Jeremy Likness <jeremy@jeremylikness.com>
2018-02-08 11:14:41 -0500

    Added image entry for table and finish logic to parse image into file and save it.

---
+++ ImageGrabber/ImageEntry.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace ImageGrabber
+{
+    public class ImageEntry : TableEntity  
+    {
+        public string BlobId { get; set; }
+        public string Caption { get; set; }
+        public string Url { get; set; }
+        public string MimeType { get; set; }
+
+        public static string GetRowKey(Uri uri)
+        {
+            return uri.AbsolutePath.Replace("/","^");
+        }
+
+        public static string GetPartitionKey(Uri uri)
+        {
+            return uri.DnsSafeHost;
+        }
+    }
+
+    
+}
--- ImageGrabber/ImageGrabberHost.cs
+++ ImageGrabber/ImageGrabberHost.cs
@@ -2,20 +2,89 @@ using EventData;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage;
+using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ImageGrabber
 {
     public static class ImageGrabberHost
     {
         [FunctionName("ImageGrabber")]
-        public static void Run([EventGridTrigger()]EventGridEvent eventPayload, TraceWriter log)
+        public static async Task Run(
+            [EventGridTrigger()]EventGridEvent eventPayload, 
+            [Table("images")]CloudTable imageTable,
+            TraceWriter log)
         {
             log.Info($"Received event: {eventPayload}");
             var imageData = eventPayload.Data.ToObject<ImageData>();
             if (imageData != null)
             {
                 log.Info($"Request to process URL: {imageData.Url}.");
+                if (Uri.TryCreate(imageData.Url, UriKind.Absolute, out Uri uri))
+                {
+                    try
+                    {
+                        var client = new HttpClient();
+                        var result = await client.GetAsync(imageData.Url);
+
+                        var mime = result.Content.Headers.ContentType.MediaType;
+
+                        if (!mime.StartsWith("image/"))
+                        {
+                            log.Warning($"Not an image: {mime}");
+                            return;
+                        }
+
+                        var id = Guid.NewGuid().ToString();
+
+                        log.Info("Get storage account.");
+                        var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["AzureWebJobsStorage"].ToString());
+
+                        log.Info("Create client.");
+                        var blobClient = storageAccount.CreateCloudBlobClient();
+
+                        log.Info("Get container reference.");
+                        var container = blobClient.GetContainerReference("images");
+
+                        log.Info("Get block blog reference.");
+                        var blob = container.GetBlockBlobReference(id);
+
+                        log.Info("Upload image stream.");
+                        await blob.UploadFromStreamAsync(await result.Content.ReadAsStreamAsync());
+                        log.Info($"Added blob with id {id}");
+
+                        var newEntry = new ImageEntry
+                        {
+                            PartitionKey = ImageEntry.GetPartitionKey(uri),
+                            RowKey = ImageEntry.GetRowKey(uri),
+                            BlobId = id,
+                            Url = uri.ToString(),
+                            MimeType = mime,
+                            Caption = string.Empty
+                        };
+                        log.Info($"Adding table: {JsonConvert.SerializeObject(newEntry)}.");
+                        var operation = TableOperation.Insert(newEntry);
+                        log.Info("Insert table.");
+                        await imageTable.ExecuteAsync(operation);
+                        log.Info($"Added table mapping.");
+                    }
+                    catch(Exception ex)
+                    {
+                        log.Error("Unexpected exception", ex);
+                        throw;
+                    }
+                }
+                else
+                {
+                    log.Warning($"Bad url: {imageData.Url}");
+                }
             }
         }
+
     }
 }
